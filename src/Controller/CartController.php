d73ac993c096164a655dc5a962b87c90eeba7d02
Frostbowl <masclet.c@gmail.com>
2024-08-13 16:23:33 +0400

    add stripe service

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Sweatshirt;
+use App\Service\StripeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Service\Reporting\ReportRunService;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -43,5 +45,51 @@ class CartController extends AbstractController
             'sweatshirts' => $sweatshirts,
             'total' => $total,
         ]);
+    }
+
+    #[Route('cart/checkout', name:'app_cart_checkout')]
+    public function checkout(EntityManagerInterface $entityManager, SessionInterface $session, StripeService $stripeService): Response
+    {
+        $cart = $session->get('cart', []);
+        $lineItems = [];
+
+        foreach($cart as $item){
+            $sweatshirt = $entityManager->getRepository(Sweatshirt::class)->find($item['id']);
+            if($sweatshirt){
+                $lineItems[]=[
+                    'price_data'=>[
+                        'currency'=>'eur',
+                        'product_data'=>[
+                            'name'=>$sweatshirt->getName() . ' - Taille ' . $item['size'],
+                        ],
+                        'unit_amount'=>$sweatshirt->getPrice() * 100,
+                    ],
+                    'quantity'=> 1,
+                ];
+            }
+        }
+
+        $successUrl = $this->generateUrl('app_cart_success', [],  \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+        $cancelUrl = $this->generateUrl('app_cart_cancel', [], \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $sessionStripe = $stripeService->createCheckoutSession($lineItems, $successUrl, $cancelUrl);
+
+        return $this->redirect($sessionStripe->url, 303);
+    }
+    
+    #[Route('/cart/success', name: 'app_cart_success')]
+    public function success(SessionInterface $session): Response
+    {
+        $session->remove('cart');
+        $homeUrl = $this->generateUrl('app_home', [], \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+        return new Response('<h1>Paiement réussi</h1><br><p><a href="'. $homeUrl . '">Retour à l\'accueil</a></p>');
+    }
+
+    #[Route('/cart/cancel', name: 'app_cart_cancel')]
+    public function cancel(): Response
+    {
+        $homeUrl = $this->generateUrl('app_home', [], \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+        // Display a cancellation message or redirect to cart
+        return new Response('<h1>Echec du paiement</h1><br><p><a href="'. $homeUrl . '">Retour à l\'accueil</a></p>');
     }
 }
